1298b209b493a2f23eb0311ee8181644b4ee01ce
Rails Udemy Course <railsudemycourse.gmail.com>
2016-04-04 14:38:27 +0000

    add admin user functionnality

--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
+  
   def new
     @user = User.new
   end
@@ -33,10 +35,17 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
+  
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -48,9 +57,15 @@ def set_user
 end
 
 def require_same_user
- if !logged_in? || current_user != @user
+ if (!logged_in? || current_user != @user) and !current_user.admin?
    flash[:danger] = "You do not own this user's account or must be logged in"
    redirect_to root_path
  end
 end
  
+def require_admin
+  if logged_in? and !current_user.admin?
+    flash[:danger] = "Only admin users can perform that action"
+    redirect_to root_path
+  end
+end
